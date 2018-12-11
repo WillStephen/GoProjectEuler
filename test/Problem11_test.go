e1156c11a635dadb6c417b5b36fec591e29c0e1e
Will Stephen <will.stephen@towerswatson.com>
2018-12-11 15:22:50 +0000

    Add test for problem 11

---
+++ test/Problem11_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"GoProjectEuler/common"
+	"GoProjectEuler/solutions"
+	"testing"
+)
+
+func TestProblem11(t *testing.T) {
+	result := solutions.RunSolution11()
+
+	common.AssertEqual(t, 70600674, result, "")
+}
+
+func BenchmarkProblem11(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		solutions.RunSolution11()
+	}
+}
